a44a0f345357b4d2bcc7fdb03e51e4efd2c5b230
ComputerCraftr <ComputerCraftr@users.noreply.github.com>
2017-11-29 22:50:50 -0700

    Create poly.h

---
+++ poly.h
@@ -0,0 +1,16 @@
+#ifndef POLY_H
+#define POLY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void poly_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
